7f485667bf47b245d387dc2db26eecd6c7018ba7
Christian Ingenhaag <christian.ingenhaag@googlemail.com>
2025-05-12 23:10:58 +0200

    better navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,18 +2,25 @@
 	import '../app.css';
 
 	let { children } = $props();
-</script>
+</script>    
 
-
-<div class="container mx-auto shadow-md p-5 mt-3 lg:w-1/2 max-w-prose bg-white">
+<div class="container mx-auto shadow-md lg:w-1/2 max-w-prose bg-white">
 	<div class="navbar bg-base-100 shadow-sm">
     <div class="flex-1">
       <a class="btn btn-ghost text-xl">Notenhelferlein</a>
     </div>
     <div class="flex-none">
-      <button class="btn btn-square btn-ghost">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block h-5 w-5 stroke-current"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path> </svg>
-      </button>
+      <div class="dropdown dropdown-end">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+      </div>
+      <ul
+        tabindex="0"
+        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li><a>Settings</a></li>
+        <li><a>About</a></li>
+      </ul>
+    </div>
     </div>
 	</div>
 	
